c21492a37bebc19d31139a14faf182b5586277e2
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-08-12 17:19:39 +0430

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <h1>app-title</h1>
+      <hr />
+      <br />
+      {/*//+ My Codes Will Be HERE ! */}
+    </div>
+  );
+};
+
+export default App;
