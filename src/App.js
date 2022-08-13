6df86a7f2cfc080f9ab50b760c7abbc24710595f
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-08-13 11:22:57 +0430

    Add random button (how to update array states)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [numbers, setNumbers] = useState([]);
+
+  const ChooseRandom = () => {
+    const newNumber = Math.floor(Math.random() * 10000);
+    setNumbers([...numbers, { id: numbers.length, value: newNumber }]);
+  };
+
   return (
     <div>
-      <h1>app-title</h1>
+      <h1>random-number</h1>
       <hr />
       <br />
-      {/*//+ My Codes Will Be HERE ! */}
+      <button onClick={ChooseRandom}>Make Random</button>
+      <ol>
+        {numbers.map((number) => {
+          return (
+            <li key={number.id}>
+              <hr />
+              <h3>{number.value}</h3>
+            </li>
+          );
+        })}
+        <hr />
+      </ol>
     </div>
   );
 };
